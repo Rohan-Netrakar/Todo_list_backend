a8529a30a85f06972f13e2f38d5bf5d9063b8926
Rohan A Netrakar <rohannetrakar25@gmail.com>
2025-09-04 14:42:56 +0530

    router created for add the task and store it

--- app.js
+++ app.js
@@ -16,12 +16,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+let taskes=[];
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index',{tasks:taskes});
 });
 
+//Add router
+
+
+app.post("/add",(req,res)=>{
+    const {task}= req.body;
+    console.log(task);
+    taskes.push(task);
+    // res.render("index",{tasks:taskes})
+    res.redirect("/");
+});
 
 // Start server
 app.listen(port, () => {
