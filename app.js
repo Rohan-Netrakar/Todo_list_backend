dd7df365bec01617d9b063547467794e8736f62b
Rohan-Netrakar <rohannetrakar25@gmail.com>
2025-09-05 16:02:23 +0530

    connected data base for add and delete task routers

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import path from "path";
-import { fileURLToPath } from "url"; 
+import { fileURLToPath } from "url";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Rohan@25",
+  port: 5432,
+});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,36 +18,80 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 3000;
 
+db.connect();
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-let taskes=[];
+//let taskes = [];
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('index',{tasks:taskes});
+app.get("/", async (req, res) => {
+  try {
+    const query = `
+      SELECT id, task
+      FROM Todo_list_backend 
+      ORDER BY id ASC
+    `;
+    const result = await db.query(query);
+    res.render("index", { tasks: result.rows });
+  } catch (err) {
+    console.error("Error retrieving Todo_list_backends", err.stack);
+    res.status(500).send("Database error");
+  }
+  
 });
 
 //Add router
-app.post("/add",(req,res)=>{
-    const {task}= req.body;
-    console.log(task);
-    taskes.push(task);
-    // res.render("index",{tasks:taskes})
+app.post("/add", async (req, res) => {
+  const { task } = req.body;
+  console.log(task);
+
+  //taskes.push(task);
+  // res.render("index",{tasks:taskes})
+  //res.redirect("/");
+
+  try {
+    const query = `
+      INSERT INTO Todo_list_backend (task)
+      VALUES ($1)
+    `;
+    await db.query(query, [task]);
+    //res.send(`${task} added successfully!`);
     res.redirect("/");
+  } catch (err) {
+    console.error("Error inserting Todo_list_backend", err.stack);
+    res.status(500).send("Database error");
+  }
+  
 });
 
-//Delete router
-app.post("/delete-first",(req,res)=>{
-    taskes.shift();
+//Delete router  "/delete/:id"
+app.post("/delete/:id",async (req, res) => {
+//   taskes.shift();
+//   res.redirect("/");
+const id = req.params.id;
+console.log(id);
+  try {
+    const query = `
+      DELETE FROM Todo_list_backend 
+      WHERE id = $1
+    `;
+    await db.query(query, [id]);
+    //res.send('Todo_list_backend deleted successfully!');
     res.redirect("/");
+  } catch (err) {
+    console.error("Error deleting Todo_list_backend", err.stack);
+    res.status(500).send('Database error');
+  }
+
 });
+
+app.post("/edit", (req, res) => {});
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
-
