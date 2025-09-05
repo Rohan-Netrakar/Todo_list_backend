77cda570affcc04cf08982f1a5bacc5192391c60
Rohan-Netrakar <rohannetrakar25@gmail.com>
2025-09-05 22:12:35 +0530

    added public path

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ db.connect();
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
