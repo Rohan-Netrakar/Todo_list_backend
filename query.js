ea6fe5e5f80015fbc7bd5e607fbc7279007a105e
Srujana Ellur <srujanaellur@gmail.com>
2025-09-05 15:32:39 +0530

    Todo_list_backend query.js

--- query.js
+++ query.js
@@ -1,51 +1,51 @@
 //to create a database table
 
-// CREATE TABLE todos (
+// CREATE TABLE Todo_list_backend (
 //   id SERIAL PRIMARY KEY,
 //   task VARCHAR(255)
 // );
 
-//to INSERT a todo item
+//to INSERT a Todo_list_backend item
 // try {
 //     const query = `
-//       INSERT INTO todos (task)
+//       INSERT INTO Todo_list_backend (task)
 //       VALUES ($1)
 //     `;
 //     await db.query(query, [task]);
-//     res.send('Todo added successfully!');
+//     res.send('Todo_list_backend added successfully!');
 //   } catch (err) {
-//     console.error("Error inserting todo", err.stack);
+//     console.error("Error inserting Todo_list_backend", err.stack);
 //     res.status(500).send('Database error');
 //   }
 // });
 
 
-//to DELETE a todo item by id
+//to DELETE a Todo_list_backend item by id
 // try {
 //     const query = `
-//       DELETE FROM todos
+//       DELETE FROM Todo_list_backend 
 //       WHERE id = $1
 //     `;
 //     await db.query(query, [id]);
-//     res.send('Todo deleted successfully!');
+//     res.send('Todo_list_backend deleted successfully!');
 //   } catch (err) {
-//     console.error("Error deleting todo", err.stack);
+//     console.error("Error deleting Todo_list_backend", err.stack);
 //     res.status(500).send('Database error');
 //   }
 // });
 
 
-//to SELECT all todo items
+//to SELECT all Todo_list_backend items
 // try {
 //     const query = `
 //       SELECT id, task
-//       FROM todos
+//       FROM Todo_list_backend 
 //       ORDER BY id ASC
 //     `;
 //     const result = await db.query(query);
 //     res.render('index', { tasks: result.rows });
 //   } catch (err) {
-//     console.error("Error retrieving todos", err.stack);
+//     console.error("Error retrieving Todo_list_backends", err.stack);
 //     res.status(500).send("Database error");
 //   }
 // });
